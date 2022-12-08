14a4cc590f05f74fad963b61638367482a77aec4
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-08 19:33:17 +0300

    added second task

---
+++ homeworkSem5/src/second.java
@@ -0,0 +1,13 @@
+//Пусть дан список сотрудников: Иван, Пётр, Антон и так далее. Написать программу,
+//        которая найдет и выведет повторяющиеся имена с количеством повторений.
+//        Отсортировать по убыванию популярности.
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class second {
+    public static void main(String[] args) {
+        List<String> staff = new ArrayList<>();
+
+}

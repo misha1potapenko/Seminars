6dd21480a53665b395cf3bd767e0c168ae2335ab
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-08 19:53:35 +0300

    added staff

--- homeworkSem5/src/second.java
+++ homeworkSem5/src/second.java
@@ -4,10 +4,30 @@
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class second {
     public static void main(String[] args) {
         List<String> staff = new ArrayList<>();
+        staff.add("Петров");
+        staff.add("Иванов");
+        staff.add("Сидоров");
+        staff.add("Володин");
+        staff.add("Сидоров");
+        staff.add("Сидоров");
+        staff.add("Петров");
+        staff.add("Курлюков");
+        System.out.println(staff);
+        Map<String,Integer> staffs = new HashMap<>();
+        int count = 0;
+        for (int i = 0; i <staff.size() ; i++) {
+            for (int j = 1; j <staff.size()-1 ; j++) {
+                if (staff.get(i).equals(staff.get(j))) count ++;
 
+            }
+
+        }
+    }
 }

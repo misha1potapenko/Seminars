198893afc04fe778e77aaaf351ee71a7ab519f4f
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-10 20:14:50 +0300

    added five notebook

--- finishProject/src/Notebook.java
+++ finishProject/src/Notebook.java
@@ -1,5 +1,25 @@
+//1 - ОЗУ
+//        2 - Объем ЖД
+//        3 - Операционная система
+//        4 - Цвет …
+
+
 public class Notebook {
+
     private String stamp;
     private String model;
+    private String RAM;
+    private String hardDiskCapacity;
+    private String opirationSystem;
+    private String color;
 
+    public Notebook(String stamp, String model, String ram, String hardDiskCapacity,
+                    String opirationSystem, String color) {
+        this.stamp = stamp;
+        this.model = model;
+        RAM = ram;
+        this.hardDiskCapacity = hardDiskCapacity;
+        this.opirationSystem = opirationSystem;
+        this.color = color;
+    }
 }

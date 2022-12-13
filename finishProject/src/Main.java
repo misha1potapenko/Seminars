ea868ea5656c2931652f8867edec625024b35f1e
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-13 21:01:49 +0300

    added fifnish filter

--- finishProject/src/Main.java
+++ finishProject/src/Main.java
@@ -19,84 +19,89 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Notebook notebook1 = new Notebook("Asus", "pavilion 326", 8,
-                512, "Mac", "white");
-        Notebook notebook2 = new Notebook("Asus", "pavilion 329", 16,
-                512, "Win10", "black");
-        Notebook notebook3 = new Notebook("HP", "426", 8,
-                1024, "Win10", "black");
-        Notebook notebook4 = new Notebook("Aser", "JPQ 876", 32,
-                1024, "Mac", "white");
-        Notebook notebook5 = new Notebook("Aser", "HDl 586", 8,
-                512, "Linux", "black");
-        Map <String, Object> forNotebook = new TreeMap();
-        forNotebook.put("notebook1", notebook1);
-        forNotebook.put("notebook2", notebook2);
-        forNotebook.put("notebook3", notebook2);
-        forNotebook.put("notebook4", notebook2);
-        forNotebook.put("notebook5", notebook2);
-        choiseNotebook();
-
-
-
-
-
-
-        }
-    public static Map<String,String> choiseNotebook() {
-        Integer choise = 0;
-        Map <String, String> forChoiseBook = new TreeMap<>();
-        while (choise!=7) {
+        Set<Notebook> laptops = new HashSet<Notebook>();
+        laptops.add(new Notebook("Asus", "pavilion 326", 8,
+                512, "Mac", "white"));
+        laptops.add(new Notebook("Asus", "pavilion 329", 16,
+                512, "Win10", "black"));
+        laptops.add(new Notebook("Asus", "pavilion 354549", 32,
+                512, "Win10", "black"));
+        laptops.add(new Notebook("Asus", "pavilion 354549", 64,
+                512, "Win10", "black"));
+        laptops.add(new Notebook("HP", "426", 8,
+                1024, "Win10", "black"));
+        laptops.add(new Notebook("HP", "4dsd26", 16,
+                1024, "Win10", "black"));
+        laptops.add(new Notebook("Aser", "JPQ 876", 32,
+                1024, "Mac", "white"));
+        laptops.add(new Notebook("Aser", "JPadadQ 876", 64,
+                1024, "Linux", "white"));
+        laptops.add(new Notebook("Mac", "HDl 586", 8,
+                512, "Linux", "black"));
+        laptops.add(new Notebook("Mac", "HDl 586", 64,
+                512, "Linux", "black"));
+        List<Notebook> filtered = new ArrayList<>();
+        List<Notebook> filtered2 = new ArrayList<>();
+        List<Notebook> filtered3 = new ArrayList<>();
+        List<Notebook> filtered4 = new ArrayList<>();
+        int choise = 0;
+        while (choise != 5) {
             System.out.println("Здравствуйте. Подскажите какой критерий выбора вас интересует: (введите цифру)\n" +
                     "1 Марка\n" +
                     "2 Объем оперативной памяти\n" +
                     "3 Размер жесткого диска\n" +
                     "4 Операционная система\n" +
-                    "5 Цвет\n" +
-                    "6 Поиск\n" +
-                    "7 Выход\n");
+                    "5 Выход\n");
             Scanner forChoise = new Scanner(System.in);
             choise = forChoise.nextInt();
-            if (choise == 1){
+            if (choise == 1) {
                 System.out.println("Введите марку: (Asus, Mac, Aser, HP)");
                 Scanner stamp = new Scanner(System.in);
                 String forStamp = stamp.nextLine();
-                forChoiseBook.put("Stamp", forStamp);
+                for (Notebook laptop : laptops) {
+                    if (laptop.getStamp().equals(forStamp))
+                        filtered.add(laptop);
 
 
+                }
+                System.out.println(filtered);
             }
-            if (choise == 2){
+            if (choise == 2) {
                 System.out.println("Введите объем оперативной памяти: (4Гб, 8Гб, 16Гб, 32Гб, 64Гб)");
-                Scanner stamp = new Scanner(System.in);
-                String forStamp = stamp.nextLine();
-                forChoiseBook.put("ram",forStamp);
+                Scanner ram = new Scanner(System.in);
+                int forRam = ram.nextInt();
+                for (Notebook laptop : filtered) {
+                    if (laptop.getRAM().equals(forRam))
+                        filtered2.add(laptop);
+                }
+                System.out.println(filtered2);
             }
-            if (choise == 3){
-                System.out.println("Введите объем жесткого диска: (512Гб, 1024Гб)");
-                Scanner stamp = new Scanner(System.in);
-                String forStamp = stamp.nextLine();
-                forChoiseBook.put("hardDiskCapacity",forStamp);
-            }
-            if (choise == 4){
+                if (choise == 3) {
+                    System.out.println("Введите объем жесткого диска: (512Гб, 1024Гб)");
+                    Scanner hardDiskCapacity = new Scanner(System.in);
+                    int forhardDiskCapacity = hardDiskCapacity.nextInt();
+                    for (Notebook laptop : filtered2) {
+                        if (laptop.getHardDiskCapacity().equals(forhardDiskCapacity))
+                            filtered3.add(laptop);
+                    }
+                    System.out.println(filtered3);
+                }
+            if (choise == 4) {
                 System.out.println("Введите операционную систему: (Linux, Win10, Mac)");
                 Scanner stamp = new Scanner(System.in);
                 String forStamp = stamp.nextLine();
-                forChoiseBook.put("opirationSystem",forStamp);
+                for (Notebook laptop : filtered2) {
+                    if (laptop.getOpirationSystem().equals(forStamp))
+                        filtered4.add(laptop);
+                }
+                System.out.println(filtered4);
             }
-            if (choise == 5){
-                System.out.println("Введите цвет: (white, black)");
-                Scanner stamp = new Scanner(System.in);
-                String forStamp = stamp.nextLine();
-                forChoiseBook.put("color",forStamp);
-            }
-            if (choise == 6){
-                System.out.println("Ищем подходящие ноутбуки: ");
-                System.out.println(forChoiseBook);
 
-            }
+                }
+
+
         }
-    return forChoiseBook;
-    }
+
     }
 
 
--- finishProject/src/Notebook.java
+++ finishProject/src/Notebook.java
@@ -62,62 +62,16 @@ public class Notebook {
         return hardDiskCapacity;
     }
 
-    public void setHardDiskCapacity(Integer hardDiskCapacity) {
-        this.hardDiskCapacity = hardDiskCapacity;
+    public String getModel() {
+        return model;
     }
-//    public static void choiseNotebook() {
-//        Integer choise = 0;
-//        ArrayList<String> forChoiseBook = new ArrayList<>();
-//        while (choise!=7) {
-//            System.out.println("Здравствуйте. Подскажите какой критерий выбора вас интересует: (введите цифру)\n" +
-//                    "1 Марка\n" +
-//                    "2 Объем оперативной памяти\n" +
-//                    "3 Размер жесткого диска\n" +
-//                    "4 Операционная система\n" +
-//                    "5 Цвет\n" +
-//                    "6 Поиск\n" +
-//                    "7 Выход\n");
-//            Scanner forChoise = new Scanner(System.in);
-//            choise = forChoise.nextInt();
-//            if (choise == 1){
-//                System.out.println("Введите марку: (Asus, Mac, Aser, HP)");
-//                Scanner stamp = new Scanner(System.in);
-//                String forStamp = stamp.nextLine();
-//                forChoiseBook.add(forStamp);
-//            }
-//            if (choise == 2){
-//                System.out.println("Введите объем оперативной памяти: (4Гб, 8Гб, 16Гб, 32Гб, 64Гб)");
-//                Scanner stamp = new Scanner(System.in);
-//                String forStamp = stamp.nextLine();
-//                forChoiseBook.add(forStamp);
-//            }
-//            if (choise == 3){
-//                System.out.println("Введите объем жесткого диска: (512Гб, 1024Гб)");
-//                Scanner stamp = new Scanner(System.in);
-//                String forStamp = stamp.nextLine();
-//                forChoiseBook.add(forStamp);
-//            }
-//            if (choise == 4){
-//                System.out.println("Введите  систему: (Linux, Win10, Mac)");
-//                Scanner stamp = new Scanner(System.in);
-//                String forStamp = stamp.nextLine();
-//                forChoiseBook.add(forStamp);
-//            }
-//            if (choise == 5){
-//                System.out.println("Введите цвет: (white, black)");
-//                Scanner stamp = new Scanner(System.in);
-//                String forStamp = stamp.nextLine();
-//                forChoiseBook.add(forStamp);
-//            }
-//            if (choise == 6){
-//                System.out.println("Ищем подходящие ноутбуки: ");
-//                System.out.println(forChoiseBook);
-//
-//
-//                }
-//            }
-//
-//        }
 
+    public String getOpirationSystem() {
+        return opirationSystem;
     }
+
+    public String getColor() {
+        return color;
+    }
+}
 
